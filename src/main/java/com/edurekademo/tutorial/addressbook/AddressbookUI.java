0a49e0acee169e621575d74880138928d45abe54
Lakshmikanthwodeyar <42336104+Lakshmikanthwodeyar@users.noreply.github.com>
2019-01-19 13:36:31 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Addressbook Release V15.0 PRODUCTION");
+        filter.setInputPrompt("Addressbook Release V15.0 PRODUCTION AFTER DOCKER IMAGE");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
